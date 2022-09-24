1a984366d5937fcd4fd9ec9421cbf858ce52b59b
MartinTeitDK <martinteit@outlook.dk>
2022-09-24 13:36:57 +0200

    Færdige opgaver til aflevering 1 samt inputsheet

---
+++ Martin/ANM/Aflevering/InputSheet.m
@@ -0,0 +1,21 @@
+% Afleverings input sheet
+%% Dette sheet er et test sheet, hvor samtlige inputs til command window er samlet i. 
+
+% Tidligere eksamen E15 Opgave 2.A: Skalprod
+%% Det er ment at funktion skal returnere skalarproduktet af to vektorer.
+% Det færdige script er vedhæftet i afleveringen. 
+
+a = [-0.8 -1.1 2.5 1.7 0.3];
+b = [-1.3 -0.9 -0.2 0.8 -1.3];
+
+% Vi tilføjer et tjek hvad ans burde være ved indførsel af matlab egen
+% skalprod funktion.
+
+C = dot(a,b);
+Opgave1 = skalprod(a,b);
+display(C)
+display(Opgave1)
+
+% Tidligere eksamen E15 Opgave 2.B: billettakst
+%% Det er ment at funktion skal returnere skalarproduktet af to vektorer.
+% Det færdige script er vedhæftet i afleveringen. 
