2c9cc481eed3961dd6958d318db31241d7aa2328
mkeri <mkeri@192.168.0.10>
2022-09-26 20:30:42 +0200

    lidt ekstra

--- Morten/tandhjul.m
+++ Morten/tandhjul.m
@@ -8,18 +8,32 @@ classdef tandhjul
     methods (Static)
         function dcd = delecirkeldiameter(N, mn, phi)
             
-            if isempty(phi)
+            if isempty(phi) && length(N)==2
                 d1 = N(1) * mn
                 d2 = N(2) * mn
-                fprintf("sd")
-                C = (d1 + d2)/2
-            elseif ~isempty(phi)
+                
+            elseif ~isempty(phi) && length(N)==2
                 d1 = N(1) * mn
                 d2 = N(2) * mn
                 
                 dg1 = d1 * cosd(phi)
                 dg2 = d2 * cosd(phi)
-            end            
+            end
+        end
+
+        function caa = center_akse_afstand(d1, d2, C)
+            
+            if ~isempty(d1) && ~isempty(d2) && isempty(C)
+            C = (d1 + d2)/2
+            disp("Udregner centerakseafstanden")
+            return
+            
+            elseif isempty(d1)
+                syms d1
+                d1 = solve((d1 + d2)/2)
+                disp("Udregner d1 ud fra d2 og C")
+                return
+            end
         end
 
     end
