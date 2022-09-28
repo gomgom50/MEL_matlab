aedfa18c5c06798640d0f1792cc1d27b0040e5c5
CHRISTOFFER-PC\Christoffer <stoffer99999@hotmail.com>
2022-09-28 11:45:32 +0200

    Fixede nogle formler samt fjerne defaults da de gav problemer

--- +MEL/Krafter_pa_tandhjul.m
+++ +MEL/Krafter_pa_tandhjul.m
@@ -19,7 +19,7 @@ classdef Krafter_pa_tandhjul
                 
                 syms W_t %laver var1 til en symbolsk variabel. 
                 disp("Udregner W_t via T, d") %printer hvad vi gør
-                eq = W_t == T/(d/2)
+                eq = W_t == T/(d/2);
                 displayFormula("W_t == T/(d/2)") %vis formlen
                 svar = solve(eq, W_t); %solve for variablen
 
@@ -29,29 +29,27 @@ classdef Krafter_pa_tandhjul
                 
                 syms T %laver T til en symbolsk variabel. 
                 disp("Udregner T via W_t, d") %printer hvad vi gør
-                eq = W_t == T/(d/2)
+                eq = W_t == T/(d/2);
                 displayFormula("W_t == T/(d/2)") %vis formlen
                 svar = solve(eq, T); %solve for variablen
                 
-            elseif isempty(var3)
+            elseif isempty(d)
                 disp("d er tom")
                 disp("løs for manglende variabel her")
                 
                 syms d %laver d til en symbolsk variabel. 
                 disp("Udregner d via W_t, T") %printer hvad vi gør
-                eq = W_t == T/(d/2)
+                eq = W_t == T/(d/2);
                 displayFormula("W_t == T/(d/2)") %vis formlen
                 svar = solve(eq, d); %solve for variablen
             end
         end
 
-        function svar = Total_kraften_W(W_t, T, d, phi_n=20, psi=0)
+        function svar = Total_kraften_W(W, W_t, phi_n, psi) % fjern T og d her de bliv ikke brugt. 
             % Total_kraften_W: Udregner W, T, d, phi_n og psi ud fra manglende
             % variabler
             %  W == W_t / (cos(phi_n) * cos(psi))
             % Input:
-            % T = Torsions momentet for akslen
-            % d = Delcirkel diameteren (pitch diameter)
             % phi_n = Angrebsvinklen typisk 20 grader
             % psi = skråvinklen på tænderne -- er 0 hvis vi har lige tænder
             % Output:
@@ -62,10 +60,9 @@ classdef Krafter_pa_tandhjul
                 
                 syms W %laver W til en symbolsk variabel. 
                 disp("Udregner W via W_t, phi_n og psi") %printer hvad vi gør
-                eq = W == W_t / (dcos(phi_n) * dcos(psi))
+                eq = W == W_t / (dcos(phi_n) * dcos(psi));
                 displayFormula("W == W_t / (cos(phi_n) * cos(psi))") %vis formlen
                 svar = solve(eq, W); %solve for variablen
-
 
             elseif isempty(W_t)
                 disp("W_t er tom")
@@ -73,7 +70,7 @@ classdef Krafter_pa_tandhjul
                 
                 syms W_t %laver W_t til en symbolsk variabel. 
                 disp("Udregner W_t via W, phi_n og psi") %printer hvad vi gør
-                eq = W == W_t / (dcos(phi_n) * dcos(psi))
+                eq = W == W_t / (dcos(phi_n) * dcos(psi));
                 displayFormula("W == W_t / (cos(phi_n) * cos(psi))") %vis formlen
                 svar = solve(eq, W_t); %solve for variablen
 
@@ -83,7 +80,7 @@ classdef Krafter_pa_tandhjul
                 
                 syms phi_n %laver phi_n til en symbolsk variabel. 
                 disp("Udregner phi_n via W, W_t og psi") %printer hvad vi gør
-                eq = W == W_t / (dcos(phi_n) * dcos(psi))
+                eq = W == W_t / (dcos(phi_n) * dcos(psi));
                 displayFormula("W == W_t / (cos(phi_n) * cos(psi))") %vis formlen
                 svar = solve(eq, phi_n); %solve for variablen
              
@@ -93,13 +90,15 @@ classdef Krafter_pa_tandhjul
                 
                 syms psi %laver psi til en symbolsk variabel. 
                 disp("Udregner psi via W, W_t og phi_n") %printer hvad vi gør
-                eq = W == W_t / (dcos(phi_n) * dcos(psi))
+                eq = W == W_t / (dcos(phi_n) * dcos(psi));
                 displayFormula("W == W_t / (cos(phi_n) * cos(psi))") %vis formlen
                 svar = solve(eq, psi); %solve for variablen            
             end
         end
 
-        function svar = Radial_kraften_Wr(W_r, W, phi_n=20)
+
+
+        function svar = Radial_kraften_Wr(W_r, W, phi_n)
             % Radial_kraften_Wr: Udregner W_r, W, phi_n ud fra manglende
             % variabler
             % W_r == W * sin(phi_n)
@@ -114,10 +113,9 @@ classdef Krafter_pa_tandhjul
                 
                 syms W_r %laver W_r til en symbolsk variabel. 
                 disp("Udregner W_r via W og phi_n") %printer hvad vi gør
-                eq = W_r == W * dsin(phi_n)
+                eq = W_r == W * dsin(phi_n);
                 displayFormula("W_r == W * sin(phi_n)") %vis formlen
                 svar = solve(eq, W_r); %solve for variablen
-
 
             elseif isempty(W)
                 disp("W er tom")
@@ -125,7 +123,7 @@ classdef Krafter_pa_tandhjul
                 
                 syms W %laver W til en symbolsk variabel. 
                 disp("Udregner W via W_r og phi_n") %printer hvad vi gør
-                eq = W_r == W * dsin(phi_n)
+                eq = W_r == W * dsin(phi_n);
                 displayFormula("W_r == W * sin(phi_n)") %vis formlen
                 svar = solve(eq, W); %solve for variablen
 
@@ -135,13 +133,13 @@ classdef Krafter_pa_tandhjul
                 
                 syms phi_n %laver phi_n til en symbolsk variabel. 
                 disp("Udregner phi_n via W og W_r") %printer hvad vi gør
-                eq = W_r == W * dsin(phi_n)
+                eq = W_r == W * dsin(phi_n);
                 displayFormula("W_r == W * sin(phi_n)") %vis formlen
                 svar = solve(eq, phi_n); %solve for variablen
             end
         end
 
-        function svar = Aksial_kraften_Wa(W_a, W, phi_n=20, psi=0)
+        function svar = Aksial_kraften_Wa(W_a, W, phi_n, psi)
             % Aksial_kraften_Wa: Udregner Wa, W, phi_n og psi ud fra manglende
             % variabler
             % W_a == W * (cos(phi_n) * cos(psi))
@@ -157,7 +155,7 @@ classdef Krafter_pa_tandhjul
                 
                 syms W_a %laver W_a til en symbolsk variabel. 
                 disp("Udregner W_a via W, phi_n og psi") %printer hvad vi gør
-                eq = W_a == W * (dcos(phi_n) * dsin(psi))
+                eq = W_a == W * (dcos(phi_n) * dsin(psi));
                 displayFormula("W_a == W * (cos(phi_n) * cos(psi))") %vis formlen
                 svar = solve(eq, W_a); %solve for variablen
 
@@ -168,7 +166,7 @@ classdef Krafter_pa_tandhjul
                 
                 syms W %laver W til en symbolsk variabel. 
                 disp("Udregner W via W_a, phi_n og psi") %printer hvad vi gør
-                eq = W_a == W * (dcos(phi_n) * dsin(psi))
+                eq = W_a == W * (dcos(phi_n) * dsin(psi));
                 displayFormula("W_a == W * (cos(phi_n) * cos(psi))") %vis formlen
                 svar = solve(eq, W_a); %solve for variablen
 
@@ -178,7 +176,7 @@ classdef Krafter_pa_tandhjul
                 
                 syms phi_n %laver phi_n til en symbolsk variabel. 
                 disp("Udregner phi_n via W, W_a og psi") %printer hvad vi gør
-                eq = W_a == W * (dcos(phi_n) * dsin(psi))
+                eq = W_a == W * (dcos(phi_n) * dsin(psi));
                 displayFormula("W_a == W * (cos(phi_n) * cos(psi))") %vis formlen
                 svar = solve(eq, phi_n); %solve for variablen
              
@@ -188,7 +186,7 @@ classdef Krafter_pa_tandhjul
                 
                 syms psi %laver psi til en symbolsk variabel. 
                 disp("Udregner psi via W, W_a og phi_n") %printer hvad vi gør
-                eq = W_a == W * (dcos(phi_n) * dsin(psi))
+                eq = W_a == W * (dcos(phi_n) * dsin(psi));
                 displayFormula("W_a == W * (cos(phi_n) * cos(psi))") %vis formlen
                 svar = solve(eq, psi); %solve for variablen    
             end
