f6f1960a9a1312d0ca5877e7c65712d6a5ff7ecd
CHRISTOFFER-PC\Christoffer <stoffer99999@hotmail.com>
2022-10-18 22:19:51 +0200

    Første Opgave 1

--- Aflevering 2/MaalFkt.m
+++ Aflevering 2/MaalFkt.m
@@ -1,4 +1,9 @@
 function MV = MaalFkt(Pl,Tr,Dst)
-N = length(Tr)
-
-MV = dot(Dst, Tr) 
+    sum = 0;
+    for i = 1:length(Pl)
+        for j = i:length(Tr)
+            sum = Dst(i,j) .* Tr(Pl(i), Pl(j)) + sum;
+        end
+    end
+    MV = sum;
+end
