91b2faab62136bdce3f061315c486d8cd9e82ca5
Mathias <Mathias@MDs-Farmville>
2022-10-18 22:25:13 +0200

    merge please

--- Aflevering 2/MaalFkt.m
+++ Aflevering 2/MaalFkt.m
@@ -1,4 +1,14 @@
 function MV = MaalFkt(Pl,Tr,Dst)
 N = length(Tr)
 
-MV = dot(Dst, Tr) 
+
+MV = 0
+
+for i = 1:3
+    calc = dot(Dst(:,i), Tr(:,Pl(i)))
+    MV =+ MV + calc 
+end
+
+
+
+% MV_test2 = Dst .* Tr
