ab7485463a9985daf1e664d39acb97ed4c864678
mkerichsen <mkerichsen@gmail.com>
2022-10-21 15:45:43 +0200

    korrekt funktion

--- Morten/Matlab/Gruppeaflevering/Maalfkt.m
+++ Morten/Matlab/Gruppeaflevering/Maalfkt.m
@@ -5,7 +5,7 @@ x = zeros(1, k);
 ctr = 1;
 
 for i = 1:length(pl)-1
-    for j = 2:length(pl)
+    for j = i+1:length(pl)
 
 
         x(ctr) = dst(i, j) * tr(pl(i), pl(j));
@@ -18,5 +18,5 @@ for i = 1:length(pl)-1
 
 end
 
-sum(x)
+MV = sum(x)
 end
