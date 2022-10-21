70cf50eaf03233bb685902cb5606abad5f6fe580
MBH88h <elgramtia425@hotmail.com>
2022-10-21 16:44:29 +0200

    minor doc string update

--- Aflevering 2/MaalFkt.m
+++ Aflevering 2/MaalFkt.m
@@ -1,5 +1,7 @@
 function MV = MaalFkt(Pl,Tr,Dst)
-% MaalFkt - Bestemmer den totale ton afstand fra råvarelager til færdigvareLager
+% MaalFkt - Bestemmer den totale score af af funktionen ved at
+% gange de to vekterorer sammen en indgang af gangen 
+% for alle rækker i og kolonner j
 % Kald: MV = MaalFkt(Pl,Tr,Dst)
 % Input:
 % Pl = Placeringen af lokationer
@@ -8,7 +10,7 @@ function MV = MaalFkt(Pl,Tr,Dst)
 % Output:
 % MV = antal i Ton Km
     sum = 0;
-    for i = 1:length(Pl)
+    for i = 1:length(Pl) 
         for j = i:length(Tr)
             sum = Dst(i,j) .* Tr(Pl(i), Pl(j)) + sum;
         end
