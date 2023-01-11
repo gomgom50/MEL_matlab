18573d8ae8cbf479633a99babc63d05e63f2039d
MBH88h <elgramtia425@hotmail.com>
2023-01-11 13:42:51 +0100

    update

--- Mathias/Num/Eksamenssæt/F22 - ord/eulode.m
+++ Mathias/Num/Eksamenssæt/F22 - ord/eulode.m
@@ -14,8 +14,7 @@ function [t,y] = eulode(dydt,tspan,y0,h)
 if nargin<4,error('at least 4 input arguments required'),end
 ti = tspan(1);tf = tspan(2);
 if ~(tf>ti),error('upper limit must be greater than lower'),end
-t = (ti:h:tf)'; 
-n = length(t);
+t = (ti:h:tf)'; n = length(t);
 % if necessary, add an additional value of t
 % so that range goes from t = ti to tf
 if t(n)<tf
@@ -26,25 +25,3 @@ y = y0*ones(n,1); %preallocate y to improve efficiency
 for i = 1:n-1 %implement Euler's method
   y(i+1) = y(i) + dydt(t(i),y(i))*(t(i+1)-t(i));
 end
-
-% eksemple på couplede
-
-% Pmidl = 185; 
-% Pmax = 214; 
-% Lmax = 125; 
-% a = 1.2; 
-% k = 1.3; 
-% d = 0.03; 
-% S0 = 24; 
-% B0 = 66; 
-% 
-%                       Herunder 2 ligniner indsættes
-% dYdt = @(t,Y) [a*(Pmidl - Pmax + k*Y(2)), Lmax*exp(-d*Y(2)) - Y(1)]; 
-% Y0 = [S0 B0]; 
-% tidsinterval = [0 5]; 
-% h = 0.25; 
-% [t,Y] = eulsys(dYdt,tidsinterval,Y0,h); 
-% S = Y(:,1); 
-% B = Y(:,2); 
-% 
-% disp(table(t,S,B,'VariableNames',{'t, uger','S, enh./uge','B, enh.'}))
