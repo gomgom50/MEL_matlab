d01c9ad33e7682409ae0b996cd9cff971639dfb7
MBH88h <elgramtia425@hotmail.com>
2023-01-13 14:46:26 +0100

    so far

--- Mathias/Num/Eksamenssæt/F21 - ord/eulode.m
+++ Mathias/Num/Eksamenssæt/F21 - ord/eulode.m
@@ -29,20 +29,22 @@ end
 
 % eksemple på couplede
 
-% A_b = 1.85; %m^2
-% A_u = 0.002; %m^2
-% g = 9.81; %m/s^2
-% h_0 = 2.1; %m
-% t_start = 0;
-% t_I = A_b / A_u * sqrt(2 * h_0 / g);
+% Pmidl = 185; 
+% Pmax = 214; 
+% Lmax = 125; 
+% a = 1.2; 
+% k = 1.3; 
+% d = 0.03; 
+% S0 = 24; 
+% B0 = 66; 
 % 
-% dYdt = @(t,Y) A_u / A_b * (sqrt(2 * g) * (sqrt(h_0) - sqrt(Y(1))) - g * A_u / A_b * t); 
-% Y0 = h_0; 
-% tidsinterval = [t_start t_I]; 
-% h = 60; 
-% [t,Y] = eulode(dYdt,tidsinterval,Y0,h); 
-% % x = Y(1) 
+%                       Herunder 2 ligniner indsættes
+% dYdt = @(t,Y) [a*(Pmidl - Pmax + k*Y(2)), Lmax*exp(-d*Y(2)) - Y(1)]; 
+% Y0 = [S0 B0]; 
+% tidsinterval = [0 5]; 
+% h = 0.25; 
+% [t,Y] = eulsys(dYdt,tidsinterval,Y0,h); 
+% S = Y(:,1); 
+% B = Y(:,2); 
 % 
-% 
-% disp(table(t,Y,'VariableNames',{'t, (s)','x, (m)'}))
-
+% disp(table(t,S,B,'VariableNames',{'t, uger','S, enh./uge','B, enh.'}))
