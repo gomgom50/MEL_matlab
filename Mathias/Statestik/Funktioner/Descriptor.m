ee5d617847ff36afb79eb3dd6e0a296498cd4a1a
MBH88h <Elgramtia425@hotmail.com>
2023-02-01 12:30:29 +0100

    hmm

--- Mathias/Statestik/Funktioner/Descriptor.m
+++ Mathias/Statestik/Funktioner/Descriptor.m
@@ -1,4 +1,9 @@
-function [middel, med, varians, stdafv, data_max, data_min] = deskriptorer(data)
+function [middel, med, varians, stdafv, data_max, data_min] = deskriptorer(data,antal_bins)
+
+interval = (ceil(max(data)) - floor(min(data)))/antal_bins
+bins = floor(min(data)):interval:ceil(max(data))
+
+histogram(data, bins, "Normalization","probability")
 
 middel = mean(data);
 med = median(data);
