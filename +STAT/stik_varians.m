d947d3a8189867ef0d38083e4a8d6c833cc67c4f
gomgom50 <stoffer99999@hotmail.com>
2023-03-01 09:25:39 +0100

    Stat

---
+++ +STAT/stik_varians.m
@@ -0,0 +1,7 @@
+function [s] = stik_varians(data)
+%Udregner stik varians (s) med data som input
+data_op = data.^2
+n = length(data)
+ss = (n*sum(data_op)-(sum(data))^2)/(n*(n-1))
+s = sqrt(ss)
+end
