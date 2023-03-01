1d941569d946f2be6b8cf1280480eb4920bdc427
MBH88h <Elgramtia425@hotmail.com>
2023-03-01 09:46:45 +0100

    ret

--- +STAT/stik_standard_afvigelse.m
+++ +STAT/stik_standard_afvigelse.m
@@ -2,6 +2,6 @@ function [s] = stik_standard_afvigelse(data)
 %Udregner stik varians (s) med data som input
 data_op = data.^2;
 n = length(data);
-ss = (n*sum(data_op)-(sum(data))^2)/(n*(n-1))
-s = sqrt(ss)
+ss = (n*sum(data_op)-(sum(data))^2)/(n*(n-1));
+s = sqrt(ss);
 end
