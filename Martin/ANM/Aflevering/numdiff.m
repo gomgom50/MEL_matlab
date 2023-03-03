7d3619d90716e90fd9bbaa92d7fa243102f3059a
MartinTeitDK <martinteit@outlook.dk>
2023-03-03 11:26:17 +0100

    Forsøg

--- Martin/ANM/Aflevering/numdiff.m
+++
@@ -1,3 +0,0 @@
-function dfdx = numdiff(f,x)
-h = 0.001;
-dfdx = (f(x+h) - f(x))/h;
--- Martin/ANM/EksamenE2022/Gamle eksamenssæt/JacFct.m
+++
@@ -1,9 +0,0 @@
-function [J,f] = JacFct(X) 
-x = X(1); y = X(2); z = X(3); 
-f = [(y^2)*exp(-x*z) - 1 
-     x + y + z - 1 
-     x*(y^2) - 4*z + (x^3)*z - 2]; 
-J = [-(y^2)*z*exp(-x*z), 2*y*exp(-x*z), -x*(y^2)*exp(-x*z)
-    1.0, 1.0, 1.0
-    3*z*(x^2)+y^2, 2*x*y, (x^3)-4];
-end
