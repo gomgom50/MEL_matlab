81c5ccec74e5af40bb11515cff2b8254aac88bd5
gomgom50 <stoffer99999@hotmail.com>
2023-03-09 11:35:26 +0100

    Jacobian gøgl

--- +STAT/Ophobningsloven.m
+++ +STAT/Ophobningsloven.m
@@ -8,9 +8,7 @@ function U_tot = Ophobningsloven(EQ, vars, varValues, varUssikerheder)
 % varValues.
 % Output:
 % U_tot = den samlede ophobning (der er taget kvadarat af den) 
-for i = 1:length(symvar(EQ))
-    ds(i) = diff(EQ, vars(i));
-end
+ds = jacobian(EQ);
 ds_num = subs(ds, vars, varValues);
 for j = 1:length(ds)
     final(j) = sqrt((ds_num(j) * varUssikerheder(j))^2);
