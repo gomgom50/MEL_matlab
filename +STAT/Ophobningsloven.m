4d3c456548113943712174e359b0db2ecd3e8f81
gomgom50 <stoffer99999@hotmail.com>
2023-03-09 10:50:55 +0100

    Ophobningsloven

---
+++ +STAT/Ophobningsloven.m
@@ -0,0 +1,19 @@
+function U_tot = Ophobningsloven(EQ, vars, varValues, varUssikerheder)
+%Udregner ophobningsloven for en given equation
+% Input:
+% EQ = Equation som skal laves ophobning om
+% vars = array med var navne (symvar(EQ))
+% varValues = Values af variablerne i samme rællefælge som vars
+% varUssikerheder = ussikerhederne i samme rækkefølge som vars og
+% varValues.
+% Output:
+% U_tot = den samlede ophobning (der er taget kvadarat af den) 
+for i = 1:length(symvar(EQ))
+    ds(i) = diff(EQ, vars(i));
+end
+ds_num = subs(ds, vars, varValues);
+for j = 1:length(ds)
+    final(j) = (ds_num(j) * varUssikerheder(j))^2;
+end
+U_tot = sqrt(sum(final)); 
+end
