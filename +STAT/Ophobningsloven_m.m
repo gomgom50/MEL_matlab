cf0956af2b61e28b54d9944a1a5b32c966af185f
MBH88h <elgramtia425@hotmail.com>
2023-03-09 22:05:34 +0100

    ny ophobning

---
+++ +STAT/Ophobningsloven_m.m
@@ -0,0 +1,40 @@
+function [y, u_y] = Ophobningsloven_m(f, vars_and_u)
+% Kald:  [y, u_y] = Ophobningsloven_m(f, vars_and_u)
+%
+% input:
+%   f = symbolsk version af funktionen
+%   vars_and_u = 2d array med symboler, måleværdier og usikkerheder fx:
+%   vars_and_u = [[A, A_val, u_A];
+%                 [k, k_val, u_k];
+%                 [T, T_val, u_T_2];
+%                 [Delta_x, Delta_x_val, u_Delta_x]];
+%
+% output:
+%   en string med funktions værdien +- usikkerheden
+%   y = værdien af funktionen (double)
+%   u_y = usikkerheden på funktionsværdien (double)
+
+bidrag_sum = 0;
+
+for i = 1:length(vars_and_u)
+    f_new = f;
+
+    for j = 1:length(vars_and_u)
+        if i ~= j
+            f_sub = subs(f_new, vars_and_u(j,1), vars_and_u(j,2));
+            f_new = f_sub;
+        end
+    end
+
+    diff_f = vpa(diff(f_new, vars_and_u(i,1)), 6);
+    bidrag = (subs(diff_f, vars_and_u(i,1), vars_and_u(i,2)) * vars_and_u(i,3))^2;
+    bidrag_sum = bidrag + bidrag_sum;
+end
+
+u_y = double(sqrt(bidrag_sum));
+y = double(subs(f, vars_and_u(:,1), vars_and_u(:,2)));
+[y, u_y];
+
+pmchar=char(177);
+fprintf(['%.1f' pmchar '%.2f'],y, u_y)
+end
