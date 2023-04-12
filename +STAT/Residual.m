2fab68c6961d8aa3463e2e90ae62beb09331a544
gomgom50 <stoffer99999@hotmail.com>
2023-04-12 11:25:08 +0200

    RisPlotTester

---
+++ +STAT/Residual.m
@@ -0,0 +1,57 @@
+function [anal] = untitled2(mdl, data)
+%UNTITLED2 Summary of this function goes here
+%   Detailed explanation goes here
+k = size(data, 2) - 1;
+y = data(:,k+1);
+n = size(data, 1);
+x = data(:,1:k);
+k = size(x,2);
+n = size(x,1);
+
+lev = mdl.Diagnostics.Leverage;
+rst = mdl.Residuals.Studentized;
+
+lev_limit = 2*(k+1)/n;
+rst_limit = 3;
+
+lev_limit_array = zeros(1, length(lev));
+rst_limit_array = zeros(1, length(rst));
+
+
+for i = 1:length(lev)
+    if lev_limit < lev(i)
+        lev_limit_array(i) = 1;
+    end
+end
+
+for i = 1:length(rst)
+    if rst_limit < rst(i)
+        rst_limit_array(i) = 1;
+    end
+end
+
+[yhat, yci] = predict(mdl, x);
+resid = y - yhat;
+
+figure(1)
+scatter(yhat, resid, 30, "filled")
+ylabel("e");
+xlabel("yhat");
+
+figure(2)
+scatter(x, resid, 30, "filled")
+ylabel("e")
+xlabel("x")
+
+levrage = lev_limit_array';
+outlier = rst_limit_array'; 
+
+anal.data = table(x, y, lev, rst, levrage, outlier, yhat, yci, resid);
+
+anal.k = k;
+anal.n = n;
+anal.lev_limit = lev_limit;
+anal.rst_limit = rst_limit;
+
+
+
