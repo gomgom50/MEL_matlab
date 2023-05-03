ff899fa5afafa4f42eee0356397344e7f4a45416
gomgom50 <stoffer99999@hotmail.com>
2023-05-03 20:53:01 +0200

    Delete Start.m

--- +MEL/Start.m
+++
@@ -1,5 +0,0 @@
-function [start] = Start()
-setenv('OPENAI_API_KEY','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')
-role = "You are a MATLAB expert who only answers with code with no explanation.";
-start = chatGPT(model="gpt-3.5-turbo", role=role);
-end
