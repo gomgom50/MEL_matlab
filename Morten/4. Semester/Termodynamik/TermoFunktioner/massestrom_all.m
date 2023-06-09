743f8518586bd6af000e61102342e17b8e9330b9
mkerichsen <mkerichsen@gmail.com>
2023-06-09 12:30:10 +0200

    123

--- Morten/4. Semester/Termodynamik/TermoFunktioner/massestrom_all.m
+++ Morten/4. Semester/Termodynamik/TermoFunktioner/massestrom_all.m
@@ -2,17 +2,17 @@ function OP = massestrom_all(phi0, qmr, h1, h4)
 
 % Det input der skal findes sættes til nan
 
-syms Phi_0 q_mR h_1 h_4
+syms Phi q_mR h_1 h_4
 
 inputs = [phi0, qmr, h1, h4];
-symbols = [Phi_0 q_mR h_1 h_4];
+symbols = [Phi q_mR h_1 h_4];
 
 
 var = isnan(inputs);
 ind = find(var==1);
 
 disp("Energiligningen opstilles:")
-eq = q_mR*(h_1 - h_4) + Phi_0 == 0
+eq = q_mR*(h_1 - h_4) + Phi == 0
 
 disp("---------------------------------------------------")
 
@@ -31,7 +31,7 @@ disp("Resultat og brugte værdier ses i nedenstående skema")
 inputs(ind) = OP;
 
 
-names = ["Phi 0";"Massestrøm";"Entalpi h1";"Entalpi h4"];
+names = ["Phi ";"Massestrøm";"Entalpi h1";"Entalpi h4"];
 units = ["W";"kg/s";"J/kg";"J/kg"];
 
 disp(table(names,round(inputs,2)',units,VariableNames=["Variabel","Størrelse","Enhed"]))
